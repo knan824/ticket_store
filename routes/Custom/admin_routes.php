baf81aadbb5bcf98f0fac625621ee908175f1bf0
Anan <youssef.i.anan@hotmail.com>
2024-10-10 20:25:22 +0300

    organizer implement

--- routes/Custom/admin_routes.php
+++ routes/Custom/admin_routes.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\EventController;
+use App\Http\Controllers\Admin\OrganizerController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('events', EventController::class);
+Route::apiResource('organizers', OrganizerController::class);
--- routes/Custom/website_routes.php
+++ routes/Custom/website_routes.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\website\eventController;
+use App\Http\Controllers\Website\OrganizerController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('events', EventController::class);
+Route::apiResource('organizers', OrganizerController::class);
