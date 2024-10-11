b590a175f2c8d3c58405481bd7becbda80ce284b
Anan <youssef.i.anan@hotmail.com>
2024-10-11 14:46:37 +0300

    ticket implement

---
+++ app/Http/Resources/Website/TicketResourse.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Website;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResourse extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => $this->type,
+            'price' => $this->price,
+            'quantity' => $this->quantity,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             EventSeeder::class,
             OrganizerSeeder::class,
             CategorySeeder::class,
+            TicketSeeder::class,
         ]);
     }
 }
